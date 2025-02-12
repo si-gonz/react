14054242e5026a6e7c56efb46a51c5f93beaf062
si.gonz <you@example.com>
2025-02-12 11:56:18 -0300

    :tada: Commit inicial

---
+++ blogpessoal/src/App.tsx
@@ -0,0 +1,14 @@
+import Home from "./pages/home/home"
+
+function App() {
+  
+
+  return (
+  <>
+  <Home/>
+  </>
+  )
+}
+
+export default App
+// componente principal, tudo que colocar aqui dentro será exibido no navegador
